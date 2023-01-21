1a176a1a9797dd5d6dc771f1223e66f48f2aacd0
Anyalee610 <anayalambright610@gmail.com>
2023-01-21 17:29:26 -0500

    deploy

--- db.js
+++ db.js
@@ -1,18 +1,14 @@
-require('dotenv').config()
-const { Pool } = require('pg')
+//get the postgres object
+const { Pool } = require('pg');
 
-const connectionDevelopment = {
-    database: process.env.DATABASE,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    port: 5432
+//get the postgres database from the 
+const pool = new Pool({
+    database: 'railway',
+    user:     '',
+    password: ''
+})
+
+
+module.exports = {
+    pool
 }
-
-const connectionProduction = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {rejectUnauthorized: false}
-  }
-
-const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionDevelopment)
-
-module.exports = pool
